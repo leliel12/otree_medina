735f37c11060cc19b95ae639314ac84df8f0fd8c
Juan B Cabral <jbc.develop@gmail.com>
2016-04-13 21:46:36 -0300

    fix

---
+++ section2/management/commands/status.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+from __future__ import print_function, unicode_literals
+
+import csv
+import argparse
+import logging
+import itertools
+
+from django.core.management.base import BaseCommand
+
+from otree.models import Session
+
+
+# =============================================================================
+# LOGGER
+# =============================================================================
+
+logger = logging.getLogger('otree')
+
+VERSION = "0.1"
+
+# =============================================================================
+# COMMND
+# =============================================================================
+
+class Command(BaseCommand):
+    help = ("List al available sessions")
+
+    def handle(self, **options):
+        print("oTree Medina Version: " + VERSION)
